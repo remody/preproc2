135da91fd9dec1d9ecc8888e6c0bf9e51f52505b
Ubuntu <ubuntu@ip-172-31-41-95.ap-northeast-2.compute.internal>
2019-05-25 07:49:53 +0000

    final?

--- elastic.py
+++ elastic.py
@@ -1,63 +1,51 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from tika import parser\n",
-    "import re\n",
-    "import sys\n",
-    "import json\n",
-    "\n",
-    "def delete_newline(text):\n",
-    "    pattern=re.compile('\\n')\n",
-    "    text=re.sub(pattern,'',text)\n",
-    "    return text\n",
-    "\n",
-    "def elastic(path):\n",
-    "    data = {}\n",
-    "    text= parser.from_file(path)\n",
-    "    pattern= re.compile('- \\d* -')\n",
-    "    text= delete_newline(text['content'])\n",
-    "    text= re.sub(pattern,'',text)\n",
-    "    length=(len(text)//15000)+1\n",
-    "    for idx in range(length):\n",
-    "        if idx ==length-1:\n",
-    "            data[idx]=text[idx*15000:]\n",
-    "        else:\n",
-    "            data[idx]=text[(idx)*15000:(idx+1)*15000]\n",
-    "    path=path[0:len(path)-4]+'_elastic'+\".json\"\n",
-    "    fp=open(path,'w')\n",
-    "    json.dump(data,fp,ensure_ascii=False)\n",
-    "    fp.close()\n",
-    "    \n",
-    "if __name__==\"__main__\":\n",
-    "#     elastic(sys.argv[1])\n",
-    "    elastic('./nonmun.pdf')\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.5.2"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from tika import parser
+import re
+import sys
+import json
+import os
+
+def delete_newline(text):
+    pattern=re.compile('\n')
+    text=re.sub(pattern,'',text)
+    return text
+
+def elastic(argv):
+    data = {}
+    os.rename(argv[1],'temp.pdf')
+    
+    jsonpath=argv[1][0:len(argv[1])-4]"_elastic"+".json"
+    pathlist=argv[1].split('/')
+    pathlist[len(pathlist)-1]='temp.pdf'
+    pdfpath='/'.join(pathlist)
+    
+    text= parser.from_file(pdfpath)
+    
+    pattern= re.compile('- \d* -')
+    text= delete_newline(text['content'])
+    text= re.sub(pattern,'',text)
+    if sys.getsizeof(text)>31999:
+        print("NO")
+        return
+    data["body"]=text
+    data["title"]=argv[2]
+    data["creator"]=argv[3]
+    data["belong"]=argv[4]
+    data["data"]=argv[5]
+    fp=open(jsonpath,'w')
+    json.dump(data,fp,ensure_ascii=False)
+    fp.close()
+    os.remove(pdfpath)
+    print("YES")
+    
+if __name__=="__main__":
+    if len(sys.argv) == 6:
+        elastic(sys.argv)
+    else:
+        print('Error : check your arg')
+
--- preproc.py
+++ preproc.py
@@ -1,107 +1,71 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "269\n",
-      "269\n"
-     ]
-    }
-   ],
-   "source": [
-    "from tika import parser\n",
-    "from konlpy.tag import Kkma\n",
-    "from hanspell import spell_checker\n",
-    "import re\n",
-    "from PyPDF2 import PdfFileReader\n",
-    "from pdfrw import PdfReader\n",
-    "import sys\n",
-    "import json\n",
-    "\n",
-    "def init(path):\n",
-    "    text=parser.from_file(path)\n",
-    "    text = delete_newline(text['content'])\n",
-    "    sentence=sentences(text)\n",
-    "    delete_index(sentence)\n",
-    "    spell_check(sentence)\n",
-    "    path=path[0:len(path)-4]+\".txt\"\n",
-    "    fp=open(path,'w')\n",
-    "    data={'sentence':sentence}\n",
-    "    print(data,file=fp)\n",
-    "    fp.close()\n",
-    "    \n",
-    "# def getText(text):\n",
-    "#     sentences=sentences(text)\n",
-    "#     text= \"\"\n",
-    "#     with open(path,encoding='utf=8') as fp:\n",
-    "#         while True:\n",
-    "#             line = fp.readline()\n",
-    "#             if not line:\n",
-    "#                 break\n",
-    "#             else:\n",
-    "#                 text=text+line\n",
-    "#     fp.close()\n",
-    "#     return text\n",
-    "\n",
-    "def spell_check(sentence):\n",
-    "    print(len(sentence))\n",
-    "    pattern=re.compile(' ')\n",
-    "    pattern2=re.compile('-\\d*-')\n",
-    "    for idx in range(len(sentence)):\n",
-    "        sentence[idx]=re.sub(pattern,'',sentence[idx])\n",
-    "        sentence[idx]=re.sub(pattern2,'',sentence[idx])\n",
-    "        result=spell_checker.check(sentence[idx])\n",
-    "        sentence[idx]=result.checked.replace(u'\\xa0','')\n",
-    "    print(len(sentence))\n",
-    "    \n",
-    "def delete_newline(text):\n",
-    "    pattern=re.compile('\\n')\n",
-    "    text=re.sub(pattern,'',text)\n",
-    "    return text\n",
-    "\n",
-    "def sentences(text):\n",
-    "    kkma=Kkma()\n",
-    "    sentence=kkma.sentences(text)\n",
-    "    del kkma\n",
-    "    return sentence\n",
-    "\n",
-    "def delete_index(sentence):\n",
-    "    r=re.compile(\"목차|목 차\")\n",
-    "    for idx in range(len(sentence)):\n",
-    "        if r.search(sentence[idx]) is not None:\n",
-    "            del sentence[idx]\n",
-    "            break \n",
-    "if __name__==\"__main__\":\n",
-    "#     init(sys.argv[1])\n",
-    "    init('./nmg.pdf')"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.5.2"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from tika import parser
+from konlpy.tag import Kkma
+from hanspell import spell_checker
+from PyPDF2 import PdfFileReader
+from pdfrw import PdfReader
+import re
+import sys
+import json
+import os
+
+def init(path):
+    os.rename(path,'temp.pdf')
+    
+    txtpath=path[0:len(path)-4]+".txt"
+    pathlist=path.split('/')
+    pathlist[len(pathlist)-1]='temp.pdf'
+    pdfpath='/'.join(pathlist)
+    
+    
+    text=parser.from_file(pdfpath)
+    text=delete_newline(text['content'])
+    
+    sentence=sentences(text)
+    delete_index(sentence)
+    spell_check(sentence)
+    
+    fp=open(txtpath,'w')
+    print(sentence,file=fp)
+    fp.close()
+    os.remove(pdfpath)
+    print(txtpath)
+
+def spell_check(sentence):
+    pattern=re.compile(' ')
+    pattern2=re.compile('-\d*-')
+    for idx in range(len(sentence)):
+        sentence[idx]=re.sub(pattern,'',sentence[idx])
+        sentence[idx]=re.sub(pattern2,'',sentence[idx])
+        result=spell_checker.check(sentence[idx])
+        sentence[idx]=result.checked.replace(u'\xa0','')
+    
+def delete_newline(text):
+    pattern=re.compile('\n')
+    text=re.sub(pattern,'',text)
+    return text
+
+def sentences(text):
+    kkma=Kkma()
+    sentence=kkma.sentences(text)
+    del kkma
+    return sentence
+
+def delete_index(sentence):
+    r=re.compile("목차|목 차")
+    for idx in range(len(sentence)):
+        if r.search(sentence[idx]) is not None:
+            del sentence[idx]
+            break 
+
+if __name__=="__main__":
+    if len(sys.argv)==2:
+        init(sys.argv[1])
+    else:
+        print('error: arg is too much or less')
+
